9b9f032164b0cc3e004051f634216aed0025e887
Nícolas Moura <n.mouramd@gmail.com>
2018-03-16 17:32:24 -0300

    initial commit :)

---
+++ src/main/java/com/nclsmr/fastkapi/FastkApiApplication.java
@@ -0,0 +1,12 @@
+package com.nclsmr.fastkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastkApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nclsmr/fastkapi/FastkApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nclsmr.fastkapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FastkApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
